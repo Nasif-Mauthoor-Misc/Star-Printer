e9ad16ac431dab54951eea7ccdf5a7a8c67fe496
MoonJumpMania <moonjumpmania@gmail.com>
2021-01-15 00:10:22 -0500

    Improved triangle print

--- main.c
+++ main.c
@@ -2,12 +2,21 @@
 
 int main()
 {
-    char star = '*';
-    short int numLines = 10;
+    const char star = '*';
+    const short int numLines = 10;
 
     for (int i = 0; i < numLines; ++i)
     {
         for (int j = 0; j <= i; ++j)
+        {
+            printf("%c", star);
+        }
+        printf("\n");
+    }
+
+    for (int i = numLines-1; i > 0; --i)
+    {
+        for (int j = i; j > 0; --j)
         {
             printf("%c", star);
         }
